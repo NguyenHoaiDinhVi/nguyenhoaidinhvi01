822f147f11b81859a2c50159e9f5314401961a50
NguyenHoaiDinhVi <tudue610@gmail.com>
2024-08-22 19:49:11 +0700

    Add files via upload

---
+++ string.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 19:34:40 2024
+
+@author: Student
+"""
+
+print("bài 1")
+a= "Đại học Quốc gia,Khu phố 6,P.Linh Trung,Q.Thủ Đức,Tp.HCM"
+print("Đại học Quốc gia\n Khu phố 6\n P. Linh Trung\n Q. Thủ Đức\n Tp.HCM")
+print("bài 2")
+print(a.replace("P.","").replace("Q.","").replace("Tp.","").replace(",","\n"))
